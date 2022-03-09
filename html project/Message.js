dbb4933a2b0a0ed1b199be34bd524e681446d13e
Bread? <75400005+DaBread-os@users.noreply.github.com>
2022-03-09 12:42:40 -0600

    Add files via upload

---
+++ html project/Message.js
@@ -0,0 +1,12 @@
+function AlertIcon(){
+          alert("yes?");     
+}
+function func1(){
+    window.location.href = "MainWebPage.html"; 
+}
+function func2(){
+    window.location.href = "WebPageAbout.html";  
+}
+function func3(){
+    window.location.href = "Help.html";     
+}
---
+++ html project/startup.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+    window.location.href = "MainWebPage.html";
+}, 10000)
